dc23b30eddc44720b811c96ddcf72aca168f988c
Sebastian Theophil <sebastian@theophil.net>
2016-07-22 22:03:51 +0200

    Read from stdin & serial port using boost::asio with timeout (1)

--- raspberry/main.cpp
+++ raspberry/main.cpp
@@ -14,6 +14,7 @@ using namespace std::chrono_literals;
 
 #include <boost/asio.hpp>
 #include <boost/range/algorithm/for_each.hpp>
+#include <boost/program_options.hpp>
 
 struct SConfigureStdin {
 	termios m_termOld;
@@ -34,112 +35,237 @@ struct SConfigureStdin {
 	}
 };
 
-int main(int nArgs, char* aczArgs[]) {
-	// TODO: Setup boost::asio TCP server to send map bitmaps?
+enum ECalibration {
+	ecalibrationUNKNOWN,
+	ecalibrationINPROGRESS,
+	ecalibrationWAITFORUSER,
+	ecalibrationDONE
+};
 
-	if(nArgs<3) {
-		std::cout << "Syntax: robot <ttyDevice> <logfile>\n";
+struct SRobotConnection : SConfigureStdin {
+	SRobotConnection(boost::asio::io_service& io_service, std::string const& strPort, bool bManual)
+		: m_stdin(io_service, ::dup(STDIN_FILENO))
+		, m_serial(io_service, strPort) // throws boost::system::system_error
+		, m_timer(io_service, boost::posix_time::seconds(60))
+        , m_bManual(bManual)
+	{
+		wait_for_command();
+		wait_for_sensor_data();
+
+		// Send synchronous connection request
+		auto SendCommand = [&](SRobotCommand const& cmd) {
+			VERIFYEQUAL(boost::asio::write(m_serial, boost::asio::buffer(&cmd, sizeof(decltype(cmd)))), sizeof(decltype(cmd)));
+		};
+
+		std::cout << "Resetting Controller\n";
+		SendCommand(SRobotCommand::reset()); // throws boost::system:::system_error
+		std::this_thread::sleep_for(1s);
+
+	 	std::cout << "Connecting to Controller\n";
+		SendCommand(SRobotCommand::connect()); // throws boost::system:::system_error
+    }
+
+    ~SRobotConnection() {
+    	// The robot connection must only be destroyed once the io_service has
+    	// completed all requests.
+    	ASSERT(m_bShutdown);
+    }
+
+    void wait_for_command() {
+		// Read a character of input
+    	boost::asio::async_read(
+    		m_stdin, 
+    		boost::asio::buffer(&m_ch, sizeof(char)),
+    		[&](boost::system::error_code const& ec, std::size_t length) {
+    			ASSERT(!ec);
+    			ASSERT(!m_bShutdown);
+
+    			switch(m_ch) {
+	    			case 'w': 
+	    				if(m_bManual) {
+	    					send_command(SRobotCommand::forward()); 
+	    				}
+	    				break;
+					case 'a': 
+						if(m_bManual) {
+							send_command(SRobotCommand::left_turn()); 
+						}
+						break;
+					case 's': 
+						if(m_bManual) {
+							send_command(SRobotCommand::backward()); 
+						}
+						break;
+					case 'd':
+						if(m_bManual) {
+							send_command(SRobotCommand::right_turn()); 
+						}
+						break;
+					case 'c': 
+						if(ecalibrationWAITFORUSER==m_ecalibration) {
+							m_ecalibration=ecalibrationDONE; 
+						}
+						break;
+					case 'x': 
+						Shutdown();
+						return; // Don't wait for further commands
+				}
+
+			    wait_for_command();
+    		});
+    }
+
+    void wait_for_sensor_data() {
+    	m_timer.async_wait([this](boost::system::error_code const& ec) {
+            ASSERT(!ec);
+    		Shutdown(); // No data in 60s -> shutdown
+    	});
+
+    	boost::asio::async_read(
+    		m_serial, 
+    		boost::asio::buffer(&m_sensordata, sizeof(SSensorData)),
+    		[&](boost::system::error_code const& ec, std::size_t length) {
+    			ASSERT(!ec);
+    			ASSERT(length==sizeof(SSensorData));
+
+    			// Ignore further data if we're waiting for the last reset command to be delivered
+    			if(m_bShutdown) return;
+
+    			switch(m_ecalibration) {
+					case ecalibrationUNKNOWN:
+						if(m_sensordata.m_nYaw!=USHRT_MAX) {
+							std::cout << "To calibrate accelerometer, move robot in an 8." << std::endl;
+							m_ecalibration = ecalibrationINPROGRESS;
+						} else {
+							m_ecalibration = ecalibrationDONE; // No accelerometer, nothing to calibrate
+						}
+						break;
+					case ecalibrationINPROGRESS: {
+						auto tpNow = std::chrono::system_clock::now();
+						std::chrono::duration<double> durDiff = tpNow-m_tpLastMessage;
+						if(5.0 < durDiff.count()) {					
+							std::cout << "Calibration values: " << static_cast<int>(m_sensordata.m_nCalibSystem) << ", " 
+								<< static_cast<int>(m_sensordata.m_nCalibGyro) << ", " 
+								<< static_cast<int>(m_sensordata.m_nCalibAccel) << ", "
+								<< static_cast<int>(m_sensordata.m_nCalibMag) << std::endl;
+							m_tpLastMessage = tpNow;
+						}
+						if(3==m_sensordata.m_nCalibSystem) {
+							std::cout << "Calibration succeeded. Put robot on the floor and press c." << std::endl;
+							m_ecalibration = ecalibrationWAITFORUSER;
+						}
+						break;
+					}
+					case ecalibrationWAITFORUSER: {
+						break; // do nothing until user presses button
+					}
+					case ecalibrationDONE:
+					{
+					}
+				}
+
+				wait_for_sensor_data();
+    		}); 
+    }
+
+    void send_command(SRobotCommand const& rcmd) {}
+
+private:
+	void Shutdown() {
+		ASSERT(!m_bShutdown);
+		m_bShutdown = true;
+		send_command(SRobotCommand::reset());
+	}
+
+	bool m_bShutdown = false;
+
+  	boost::asio::posix::stream_descriptor m_stdin;
+  	char m_ch;
+
+	boost::asio::serial_port m_serial;
+	boost::asio::deadline_timer m_timer;
+    std::chrono::system_clock::time_point m_tpLastMessage;
+    
+	SSensorData m_sensordata;
+
+    bool const m_bManual;
+	ECalibration m_ecalibration = ecalibrationUNKNOWN;
+};
+
+constexpr char c_szPORT[] = "port";
+constexpr char c_szLOG[] = "log";
+constexpr char c_szMANUAL[] = "manual";
+constexpr char c_szINPUT[] = "input-file";
+
+int main(int nArgs, char* aczArgs[]) {
+	namespace po = boost::program_options;
+
+	po::options_description optdesc("Allowed options");
+	optdesc.add_options()
+	    ("help", "Print help message")
+	    (c_szPORT, po::value<std::string>(), "The serial port that is connected to the robot.")
+	    (c_szLOG, po::value<std::string>(), "Log file")
+	    (c_szMANUAL, "Control robot manually")
+	    (c_szINPUT, po::value<std::string>(), "Log file to read sensor data from");
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(nArgs, aczArgs, optdesc), vm);
+	po::notify(vm);    
+	
+	if(vm.count(c_szINPUT)) {
+		// read from log 
+		// TODO
+	} else if(vm.count(c_szPORT)) {
+		auto const strPort = vm[c_szPORT].as<std::string>();
+		std::string strLog;
+		if(vm.count(c_szLOG)) {
+			strLog = vm[c_szLOG].as<std::string>();
+		}
+		bool const bManual = vm.count(c_szMANUAL);
+
+		// TODO: Setup boost::asio TCP server to send map bitmaps?
+		// Should that be an boost::asio::io_service running on a separate thread?
+
+		try {
+			boost::asio::io_service io_service;	
+
+			std::basic_ofstream<char> ofsLog;
+			if(!strLog.empty()) {
+				ofsLog.open(strLog, std::ios_base::binary | std::ios_base::out | std::ios_base::trunc);
+				VERIFY(ofsLog.good());
+			}
+
+			auto tpStart = std::chrono::system_clock::now();
+			SRobotConnection rc(io_service, strPort, bManual); 
+				// [&](SSensorData const& data) {
+				// 	if(!strLog.empty()) {
+				// 		auto tpEnd = std::chrono::system_clock::now();
+				// 		std::chrono::duration<double> durDiff = tpEnd-tpStart;
+
+				// 		ofsLog << durDiff.count() << ";" 
+				// 			<< data.m_nYaw << ";"
+				// 			<< data.m_nAngle << ";"
+				// 			<< data.m_nDistance;
+							
+				// 		boost::for_each(data.m_anEncoderTicks, [&](short nEncoderTick) {
+				// 			ofsLog << ";" << nEncoderTick; 
+				// 		});
+				// 		ofsLog << '\n';
+				// 	}
+
+				// 	// TODO: Pass sensordata to robot controller, let robot controller queue sensor data
+				// 	// if necessary
+				// 	// TODO: if(!bManual) return controller's command to robot
+
+				// }); // throws boost::system:::system_error
+			io_service.run();
+		} catch(boost::system::system_error const& s) {
+			std::cerr << s.what();
+			return 1;
+		} 
+		return 0;	
+	} else {
+		std::cerr << "You must specify either the port to read from or an input file to parse" << std::endl;
 		return 1;
 	}
-	
-	boost::asio::io_service io_service;
-	std::cout << "Opening " << aczArgs[1] << "\n";
-	boost::asio::serial_port serial(io_service, aczArgs[1]);
-	serial.set_option(boost::asio::serial_port::baud_rate(230400));
-	
-	auto SendCommand = [&](SRobotCommand const& cmd) {
-		VERIFYEQUAL(boost::asio::write(serial, boost::asio::buffer(&cmd, sizeof(decltype(cmd)))), sizeof(decltype(cmd)));
-	};
-	
-	std::cout << "Opening " << aczArgs[2] << "\n";
-	std::basic_ofstream<char> ofsLog(aczArgs[2], std::ios_base::binary | std::ios_base::out | std::ios_base::trunc);
-	VERIFY(ofsLog.good());
-	
-	std::cout << "Resetting Controller\n";
-	SendCommand(SRobotCommand::reset()); // throws boost::system:::system_error
-    std::this_thread::sleep_for(1s);
-
-	std::cout << "Connecting to Controller\n";
-	SendCommand(SRobotCommand::connect()); // throws boost::system:::system_error
-	
-	std::atomic<bool> bRunning{true};
-
-	enum ECalibration {
-		ecalibrationUNKNOWN,
-		ecalibrationINPROGRESS,
-		ecalibrationWAITFORUSER,
-		ecalibrationDONE
-	};
-	std::atomic<ECalibration> ecalibration{ecalibrationUNKNOWN};
-
-	// Command loop
-	auto f = std::async(std::launch::async, [&] {
-		std::cout << "Waiting for commands\n";
-		SConfigureStdin s;
-		while(true) {
-			switch(static_cast<char>(std::getchar())) {
-				case 'w': SendCommand(SRobotCommand::forward()); break;
-				case 'a': SendCommand(SRobotCommand::left_turn()); break;
-				case 's': SendCommand(SRobotCommand::backward()); break;
-				case 'd': SendCommand(SRobotCommand::right_turn()); break;
-				case 'x': SendCommand(SRobotCommand::reset()); bRunning = false; return;
-				case 'c': std::cout << "C!" << std::endl; if(ecalibrationWAITFORUSER==ecalibration) ecalibration=ecalibrationDONE; break;
-			}
-		}
-	});
-	
-	// Sensor loop
-	auto tpStart = std::chrono::system_clock::now();
-	std::chrono::time_point<std::chrono::system_clock> tpLastMessage;
-	while(bRunning) {
-		// TODO: This needs a time out!
-		SSensorData data;
-		VERIFYEQUAL(boost::asio::read(serial, boost::asio::buffer(&data, sizeof(SSensorData))), sizeof(SSensorData)); // throws boost::system::system_error
-		
-		switch(ecalibration) {
-			case ecalibrationUNKNOWN:
-				if(data.m_nYaw!=USHRT_MAX) {
-					std::cout << "To calibrate accelerometer, move robot in an 8." << std::endl;
-					ecalibration = ecalibrationINPROGRESS;
-				} else {
-					ecalibration = ecalibrationDONE; // No accelerometer, nothing to calibrate
-				}
-				break;
-			case ecalibrationINPROGRESS: {
-				auto tpNow = std::chrono::system_clock::now();
-				std::chrono::duration<double> durDiff = tpNow-tpLastMessage;
-				if(5.0 < durDiff.count()) {					
-					std::cout << "Calibration values: " << static_cast<int>(data.m_nCalibSystem) << ", " 
-						<< static_cast<int>(data.m_nCalibGyro) << ", " 
-						<< static_cast<int>(data.m_nCalibAccel) << ", "
-						<< static_cast<int>(data.m_nCalibMag) << std::endl;
-					tpLastMessage = tpNow;
-				}
-				if(3==data.m_nCalibSystem) {
-					std::cout << "Calibration succeeded. Put robot on the floor and press c." << std::endl;
-					ecalibration = ecalibrationWAITFORUSER;
-				}
-				break;
-			}
-			case ecalibrationWAITFORUSER: {
-				break; // do nothing until user presses button
-			}
-			case ecalibrationDONE:
-			{
-				auto tpEnd = std::chrono::system_clock::now();
-				std::chrono::duration<double> durDiff = tpEnd-tpStart;
-
-				ofsLog << durDiff.count() << ";" 
-					<< data.m_nYaw << ";"
-					<< data.m_nAngle << ";"
-					<< data.m_nDistance;
-					
-				boost::for_each(data.m_anEncoderTicks, [&](short nEncoderTick) {
-					ofsLog << ";" << nEncoderTick; 
-				});
-				ofsLog << '\n';
-			}
-		}
-	}
-	return 0;
 }
